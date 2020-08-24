d164aa7bd55ec9e8b6b13e640c8e6993b24c467c
kambojgurpreet <kambojgurpreet32@gmail.com>
2020-08-24 17:09:36 +0530

    completed basic header UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PersonalComponent } from './personal/personal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    LandingComponent,
+    DashboardComponent,
+    PersonalComponent
   ],
   imports: [
     BrowserModule,
